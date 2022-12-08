179e5e0f79dd898361a5e34919cc4bfe07a671e4
Vickykuppan <117091379+Vickykuppan@users.noreply.github.com>
2022-12-08 19:41:37 +0530

    Add files via upload

---
+++ ReverseTriangle.java
@@ -0,0 +1,22 @@
+public class ReverseTriangle
+{  
+public static void main(String[] args)  
+{  
+int n=5;  
+for (int i= 0; i<= n-1; i++)  
+{  
+for (int j=0; j<=i; j++)  
+{  
+System.out.print(" ");  
+}  
+for (int k=0; k<=n-1; k++)  
+{  
+System.out.print("*" + " ");  
+}  
+System.out.println();    
+
+
+}
+  
+}
+}  
